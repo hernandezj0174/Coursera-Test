27a009a35a0dbc52ff4f757fbed6448edf04708a
Jorge <hernandezj0174@gmail.com>
2023-10-18 11:35:04 -0500

    Working Checkpoint

---
+++ assignment3-solution/app.js
@@ -0,0 +1,62 @@
+(function () {
+"use strict";
+
+angular.module("NarrowItDownApp", [])
+.controller("NarrowItDownController", NarrowItDownController)
+.service("MenuSearchService", MenuSearchService)
+.directive("foundItems", FoundItemsDirective);
+
+function FoundItemsDirective () {
+    var ddo = {
+        scope: {
+            items: '<'
+        },
+        templateUrl: 'template.html'
+    };
+
+    return ddo;
+};
+
+NarrowItDownController.$inject = ["MenuSearchService"];
+function NarrowItDownController (MenuSearchService) {
+    var narrow = this;
+
+    narrow.search = function (searchTerm) {
+        var promise = MenuSearchService.getMatchedMenuItems(searchTerm);
+
+        promise.then(function (response) {
+            narrow.found = response;
+        })
+    };
+};
+
+MenuSearchService.$inject = ['$http'];
+function MenuSearchService ($http) {
+    var service = this;
+
+    service.getMatchedMenuItems = function (searchTerm) {
+        return $http ({
+            method: 'GET',
+            url: 'https://coursera-jhu-default-rtdb.firebaseio.com/menu_items.json'
+        })
+        .then(function successCallback(response) {
+            var foundItems = [];
+
+            for (const category in response.data) {
+                for (var i = 0; i < response.data[category].menu_items.length; i++) {
+                    var description = response.data[category].menu_items[i].description;
+                    if (description.includes(searchTerm)) {
+                        foundItems.push(response.data[category].menu_items[i]);
+                    }
+                }
+            }
+
+            return foundItems;
+        },
+        function errorCallback(response) {
+            console.log("Something went terribly wrong!");
+        });
+    };
+};
+
+})();
